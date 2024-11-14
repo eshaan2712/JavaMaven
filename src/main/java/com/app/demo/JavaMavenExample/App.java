4ad08279bbfc57ed21295b9d0a77e1142e2ba168
Eshaan, K <eshaan.katla@gmail.com>
2024-11-14 21:55:47 +0530

    Added Maven files into Repository

---
+++ src/main/java/com/app/demo/JavaMavenExample/App.java
@@ -0,0 +1,11 @@
+package com.app.demo.JavaMavenExample;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Eshaan!");
+        System.out.println("This is your first Maven Project");
+    }
+}
