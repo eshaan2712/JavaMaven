449d3bbbd24e2483ad6b95f2d738d990df8740db
Eshaan <140683255+eshaan2712@users.noreply.github.com>
2024-11-20 15:24:30 +0530

    Update App.java

--- src/main/java/com/app/demo/JavaMavenExample/App.java
+++ src/main/java/com/app/demo/JavaMavenExample/App.java
@@ -7,6 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Eshaan!");
         System.out.println("This is your first Maven Project");
-        System.out.prinln("This is second line of your Project");
+        System.out.println("This is second line of your Project");
     }
 }
